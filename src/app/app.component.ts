0695b7d62c777b218a94d927eb943a27d2f5650f
unknown <Joseph.H.Stoops@GMail.com>
2018-11-28 12:17:36 -0700

    Changed default Angular screen to our landing page. Made a top nav bar. Generated a Logo and favicon.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'math-it';
+  activePage = 'Home';
+  isLoggedIn = true;
+  navItems = [
+    {"name": "Lightning Round", "link": "#"},
+    {"name": "Learn", "link": "#"},
+    {"name": "Homework", "link": "#"},
+    {"name": "Quiz & Test", "link": "#"},
+    {"name": "Compete", "link": "#"},
+    {"name": "Reports", "link": "#"}
+  ];
 }
