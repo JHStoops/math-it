13bcc4e35ac76983fe0d218bff74634fb079a9d9
Joseph Stoops <Joseph.H.Stoops@gmail.com>
2018-11-29 10:02:53 -0700

    Finished the saved reports page.

--- src/app/progress-report-saved/progress-report-saved.component.ts
+++ src/app/progress-report-saved/progress-report-saved.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressReportSavedComponent implements OnInit {
 
+    savedReports = [
+        {
+            name: 'tests from August',
+            startDate: '2018-08-01',
+            endDate: '2018-08-30',
+            topic: 'all',
+            source: 'tests'
+        },
+        {
+            name: 'challenges since August',
+            startDate: '2018-08-01',
+            topic: 'all',
+            source: 'challenges'
+        },
+        {
+            name: 'Homework score since August',
+            startDate: '2018-08-01',
+            topic: 'All',
+            source: 'homework'
+        },
+
+    ];
+
   constructor() { }
 
   ngOnInit() {
+    // Grab saved reports from backend
+    // Stored in JSON blob for this assignment
   }
 
 }
